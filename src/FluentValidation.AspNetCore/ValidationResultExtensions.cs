7f3ad0d29f9509637b643de31de57181edfd3e76
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2022-08-17 13:46:03 +0100

    Add second AddToModelState overload

--- src/FluentValidation.AspNetCore/ValidationResultExtensions.cs
+++ src/FluentValidation.AspNetCore/ValidationResultExtensions.cs
@@ -36,8 +36,21 @@ public static class ValidationResultExtension {
 	/// </summary>
 	/// <param name="result">The validation result to store</param>
 	/// <param name="modelState">The ModelStateDictionary to store the errors in.</param>
+	public static void AddToModelState(this ValidationResult result, ModelStateDictionary modelState) {
+		if (!result.IsValid) {
+			foreach (var error in result.Errors) {
+				modelState.AddModelError(error.PropertyName, error.ErrorMessage);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Stores the errors in a ValidationResult object to the specified modelstate dictionary.
+	/// </summary>
+	/// <param name="result">The validation result to store</param>
+	/// <param name="modelState">The ModelStateDictionary to store the errors in.</param>
 	/// <param name="prefix">An optional prefix. If omitted, the property names will be the keys. If specified, the prefix will be concatenated to the property name with a period. Eg "user.Name"</param>
-	public static void AddToModelState(this ValidationResult result, ModelStateDictionary modelState, string prefix = null) {
+	public static void AddToModelState(this ValidationResult result, ModelStateDictionary modelState, string prefix) {
 		if (!result.IsValid) {
 			foreach (var error in result.Errors) {
 				string key = string.IsNullOrEmpty(prefix)
